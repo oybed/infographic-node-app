b0cf0b4f368099ff8c940c21cb1cb175ff00a1aa
Øystein Bedin <bedin@redhat.com>
2016-08-25 21:20:41 -0600

    Updated for use with demo env

--- ReleaseJenkinsfile.groovy
+++ ReleaseJenkinsfile.groovy
@@ -1,13 +1,12 @@
 releasePipeline{
     buildCommands = ['npm install -only=dev'] // Add this when we figure out , 'npm run-script validate']
     buildTool = 'node-0.10'
-    ocHost = 'env3-1-master.innovation.labs.redhat.com'
-	dockerRegistry = 'registry.env3-1.innovation.labs.redhat.com'
+    ocHost = 'master1.env2-1.innovation.labs.redhat.com'
+    dockerRegistry = 'registry.apps.env2-1.innovation.labs.redhat.com'
     appName = 'node-app'
-    
-	envs = [
-		[name: 'Dev', projectName: 'infographic-dev'],
-    	[name: 'Stage', projectName: 'infographic-stage' ],
-    	[name: 'Production', projectName: 'infographic-prod' ]
+    envs = [
+        [name: 'Dev', projectName: 'infographic-dev'],
+        [name: 'Stage', projectName: 'infographic-stage' ],
+        [name: 'Demo', projectName: 'infographic-demo' ]
    ]
 }
