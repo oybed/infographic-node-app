af74c4d64d8f8f7213ad13cb607d298041618c9b
Øystein Bedin <bedin@redhat.com>
2017-02-27 19:59:10 -0500

    Updated return values to be json

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
 var express = require('express');
 var cors = require('cors');
 var app = express();
 var stack = require('./lib/stack');
 var bodyParser = require('body-parser');
+
 app.use(cors());
+
 app.use(bodyParser());
+
 app.post('/stack', function (req, res) {
-	console.log(req.body);
-	console.log('in /stack route');
+	// console.log(req.body);
+	// console.log('in /stack route');
 	stack.processStack(req.body,function(err, response){
 		if (err){
 			res.status(500).send(err);
@@ -25,5 +29,5 @@ app.get('/', function (req, res) {
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 app.listen(port, function () {
-  console.log('Example app listening on port '+port+'!');
+  console.log('Application listening on port: ' + port);
 });
